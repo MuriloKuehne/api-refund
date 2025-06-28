f0ca15684af5b9ea6daedb0c0c2051cc9b851db4
Murilo <murkuehne@gmail.com>
2025-06-28 20:14:47 -0300

    Added errorHandling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-
+import { errorHandling } from "./middlwares/error-handling"
 const app = express()
 
 app.use(cors())
@@ -9,5 +9,7 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("hello world!")
 })
+
+app.use(errorHandling)
 
 export { app }
