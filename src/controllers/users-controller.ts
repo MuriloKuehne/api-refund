72e8522c56c360174a565b57a3da4468b8630085
Murilo <murkuehne@gmail.com>
2025-07-01 18:25:18 -0300

    Cadastrando Usuário no Banco

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express"
 import z from "zod"
 import { UserRole } from "@prisma/client"
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
+import { hash } from "bcrypt"
 
 class UsersController {
   async create(request: Request, response: Response) {
@@ -15,9 +18,28 @@ class UsersController {
 
     const { name, email, password, role } = bodySchema.parse(request.body)
 
-    const user = await prisma.user
+    const userWithSameEmail = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    })
 
-    response.json({ name, email, password, role })
+    if (userWithSameEmail) {
+      throw new AppError("Já existe um usuário cadastrado com esse email")
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role,
+      },
+    })
+
+    response.status(201).json({ message: "Usuário criado com sucesso!" })
   }
 }
 
