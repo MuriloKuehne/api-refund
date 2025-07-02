9164f40f4b195de9e4b9b4f5a77a37a2f76cb3e8
Murilo <murkuehne@gmail.com>
2025-07-02 10:07:42 -0300

    Validação de dados da solicitação

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -4,18 +4,18 @@ import { Category } from "@prisma/client"
 
 class RefundsController {
   async create(request: Request, response: Response) {
-    // const bodySchema = z.object({
-    //   name: z.string(),
-    //   amount: z.number(),
-    //   category: z.enum([
-    //     Category.accomodation,
-    //     Category.food,
-    //     Category.others,
-    //     Category.services,
-    //     Category.transport,
-    //   ]),
-    //   filename: z.string(),
-    // })
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, "Please inform the request's name"),
+      category: z.enum([
+        Category.accomodation,
+        Category.food,
+        Category.others,
+        Category.services,
+        Category.transport,
+      ]),
+      amount: z.number().positive("The amount needs to be greater than 0"),
+      filename: z.string().min(20),
+    })
 
     response.json({ message: "ok" })
   }
